fcc04b2459a8274e5fa75abd5560ad42362f37c0
iryanu <itss8327@gmail.com>
2026-07-17 10:46:01 +0000

    To ryan

---
+++ CSPT1-170726/Prog1.java
@@ -0,0 +1,11 @@
+void main(){
+    int x=0;
+    boolean b = true;
+    boolean f = false;
+    IO.println(b + " " + X + " "+f);
+
+    char ch = '3';
+    int y= Integer.parseInt(String.valueOf(ch));
+    IO.print(y);
+
+}
